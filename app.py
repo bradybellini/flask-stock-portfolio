dad20d542122113d04324eaf48511031369588fa
Brady Bellini <47389606+bradybellini@users.noreply.github.com>
2021-02-22 21:32:03 -0800

    base

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, escape, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', company_name='TestDriven.io')
+
+@app.route('/stocks/')
+def stocks():
+    return '<h2>Stock List...</h2>'
+
+@app.route('/hello/<message>')
+def hello_message(message):
+    return f'<h1>Welcome {escape(message)}!</h1>'
+
+@app.route('/blog_posts/<int:post_id>')
+def display_blog_post(post_id):
+    return f'<h1>Blog Post #{post_id}...</h1>'
+
+@app.route('/add_stock', methods=['GET', 'POST'])
+def add_stock():
+    if request.method == 'POST':
+        for key, value in request.form.items():
+            print(f'{key}: {value}')
+
+    return render_template('add_stock.html')

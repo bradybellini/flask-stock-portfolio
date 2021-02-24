97c8f3e672eb69f745a2fae81d522374f21e9037
Brady Bellini <47389606+bradybellini@users.noreply.github.com>
2021-02-23 23:30:11 -0800

    Good start

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+import logging
+
+from logging.handlers import RotatingFileHandler
+
+from flask.logging import default_handler
+
 from flask import (
     Flask,
     escape,
@@ -5,19 +11,31 @@ from flask import (
     request,
     session,
     redirect,
-    url_for
+    url_for,
+    flash
 ) 
 
 app = Flask(__name__)
+app.logger.removeHandler(default_handler)
+
+file_handler = RotatingFileHandler('flask-stock-portfolio.log',
+                                   maxBytes=16384,
+                                   backupCount=20)
+file_formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(filename)s:%(lineno)d]')
+file_handler.setFormatter(file_formatter)
+file_handler.setLevel(logging.INFO)
+app.logger.addHandler(file_handler)
 
 app.secret_key = 'BAD_SECRET_KEY'
 
 @app.route('/')
 def index():
+    app.logger.info('Calling the index() function.')
     return render_template('index.html')
 
 @app.route('/about')
 def about():
+    flash('Thanks for learning about this site!', 'info')
     return render_template('about.html', company_name='TestDriven.io')
 
 @app.route('/stocks/')
@@ -41,6 +59,11 @@ def add_stock():
         session['stock_symbol'] = request.form['stock_symbol']
         session['number_of_shares'] = request.form['number_of_shares']
         session['purchase_price'] = request.form['purchase_price']
+
+        flash(f"Added new stock ({ request.form['stock_symbol'] })!", 'success')
+
+        app.logger.info(f"Added new stock ({ request.form['stock_symbol'] })!")
+
         return redirect(url_for('stocks'))
 
     return render_template('add_stock.html')
